63dc8d9c8a858a319b80caeb114ca0aeb5986f8a
kaze93 <75418111+kaze93@users.noreply.github.com>
2021-01-19 13:21:07 +0300

    Update tickets.py

--- tickets.py
+++ tickets.py
@@ -4,7 +4,7 @@ for i in range(ticket):
     age = int(input('Введите возраст: '))
     if age < 18:
         cost = cost + 0
-    elif 18 <= age <= 25:
+    elif 18 <= age < 25:
         cost = cost + 990
     else: cost = cost + 1390
     if ticket > 3:
